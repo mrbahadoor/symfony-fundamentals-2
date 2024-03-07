ad71fec6247ef8b5f753836adb8f1a5116f88d0d
FIRST_NAME LAST_NAME <my_name@example.com>
2024-03-07 21:00:36 +0400

    Create a symfony service with dependency injection

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,16 +3,26 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 
 class MixRepository
 {
+    public function __construct(
+        private readonly CacheInterface $cache,
+        private readonly HttpClientInterface $httpClient)
+    {
+    }
+    
     public function findAll(): array
     {
-        return $cache->get('mixes_data', function(CacheItemInterface $cacheItem) use ($httpClient){ //use() is used to pass the $httpClient variable to the closure
+        $httpClient = $this->httpClient;
+
+        return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem){ //use() is used to pass the $httpClient variable to the closure
             $cacheItem->expiresAfter(5);
 
-            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
         });
