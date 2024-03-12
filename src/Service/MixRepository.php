2247d9edf45191ef62f29069d0fb62696f6a61b6
FIRST_NAME LAST_NAME <my_name@example.com>
2024-03-12 22:53:07 +0400

    Use attribute [Autowire()]

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,9 +3,12 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class MixRepository
 {
@@ -13,13 +16,22 @@ class MixRepository
         private readonly CacheInterface $cache,
         private readonly HttpClientInterface $githubContentClient,
         #[Autowire('%kernel.debug%')]
-        private bool $isDebug
+        private bool $isDebug,
+        #[Autowire(service: 'twig.command.debug')]
+        private DebugCommand $twigDebugCommand,
     )
     {
     }
     
     public function findAll(): array
     {
+        /**
+         * Load console output in controller
+         */
+        // $output = new BufferedOutput();
+        // $this->twigDebugCommand->run(new ArrayInput([]), $output);
+        // dd($output);
+        
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem){ //use() is used to pass the $httpClient variable to the closure
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
 
