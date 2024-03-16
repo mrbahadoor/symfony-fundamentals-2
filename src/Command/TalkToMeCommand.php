44a320dc64843ca4dbc9e05fa68e6d9f806bb08d
FIRST_NAME LAST_NAME <my_name@example.com>
2024-03-16 23:08:58 +0400

    Customize command

--- src/Command/TalkToMeCommand.php
+++ src/Command/TalkToMeCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\MixRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -16,6 +17,12 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class TalkToMeCommand extends Command
 {
+    public function __construct(
+        private MixRepository $mixRepository
+    ) {
+        parent::__construct();
+    }
+    
     protected function configure(): void
     {
         $this
@@ -50,6 +57,15 @@ class TalkToMeCommand extends Command
         // );
         // $io->caution('This is a caution message');
 
+        if($io->confirm('Do you want a mix recommendation?')){
+            $mixes = $this->mixRepository->findAll();
+            $mix = $mixes[array_rand($mixes)];
+            
+            $io->note('I recommend you to listen to: ' . $mix['title']);
+        }
+
+
         return Command::SUCCESS;
+        // return Command::FAILURE;
     }
 }
